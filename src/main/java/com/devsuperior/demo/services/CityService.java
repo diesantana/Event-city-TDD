65f9193f1e7dd0c5c5568c5d1761061cb103c4d4
Diego Santana <3c.santana@gmail.com>
2024-05-22 07:19:46 -0300

    feat(service): Implementa o método delete

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -3,9 +3,13 @@ package com.devsuperior.demo.services;
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.services.exceptions.DatabaseException;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -27,6 +31,20 @@ public class CityService {
         City newCity = new City(null, dto.getName());
         newCity = cityRepository.save(newCity);
         return new CityDTO(newCity);
+    }
+    
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if(!cityRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            cityRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) { 
+            // erro de integridade referencial
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
     
 }
